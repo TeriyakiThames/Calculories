3b79a7483edd1fdde251158b40f98b3a27e5b312
Vinita Jungwiwattanaporn <vinitajwp@gmail.com>
2026-04-13 11:22:10 +0700

    feat: add delete confirmation popup & navbar

--- apps/web/src/components/MealHistory/MealHistoryClient.tsx
+++ apps/web/src/components/MealHistory/MealHistoryClient.tsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 import useSWR from "swr";
 import MealRecordList from "./MealRecordList";
 import deleteMealRecords from "@/services/api/deleteMealRecords";
+import NavBar from "../Shared/NavBar";
 
 export default function MealHistoryClient({
   locale,
@@ -25,6 +26,7 @@ export default function MealHistoryClient({
   );
   const [view, setView] = useState("Calories");
   const [viewDisplay, setViewDisplay] = useState(t("calories", messages));
+  const [showPopup, setShowPopup] = useState(false);
 
   const {
     data,
@@ -43,12 +45,16 @@ export default function MealHistoryClient({
   );
 
   const handleDelete = async () => {
-    await deleteMealRecords({
-      ids: Object.keys(checkedList)
-        .filter((key) => checkedList[Number(key)])
-        .map(Number),
-    });
-    refreshMeal();
+    setShowPopup(false);
+    if (Object.keys(checkedList).length !== 0) {
+      await deleteMealRecords({
+        ids: Object.keys(checkedList)
+          .filter((key) => checkedList[Number(key)])
+          .map(Number),
+      });
+      setCheckedList({});
+      refreshMeal();
+    }
   };
 
   const mealRecords: MealRecord[] = data?.data;
@@ -59,7 +65,7 @@ export default function MealHistoryClient({
       <div className="text-grey-100 flex items-center justify-between">
         {isEditing ? (
           <button
-            className="hover:bg-grey-10 rounded-xl p-2"
+            className="hover:bg-grey-10 rounded-xl p-2 hover:cursor-pointer"
             onClick={() => {
               setIsEditing(false);
               setCheckedList({}); // reset checkboxes
@@ -73,15 +79,15 @@ export default function MealHistoryClient({
         <h1 className="pb-1 text-xl font-bold">Meal History</h1>
         {isEditing ? (
           <button
-            className="hover:bg-grey-10 rounded-xl p-2"
-            onClick={() => handleDelete()}
+            className={`hover:bg-grey-10 disabled:text-grey-40 rounded-xl p-2 text-red-100 transition-all ${Object.keys(checkedList).every((r) => !checkedList[+r]) ?? "hover:cursor-pointer"}`}
+            onClick={() => setShowPopup(true)}
+            disabled={Object.keys(checkedList).every((r) => !checkedList[+r])}
           >
             Delete
           </button>
         ) : (
           <button
             className="hover:bg-grey-10 rounded-xl p-2"
-            // TODO: setIsEditing(true) and has Delete button
             onClick={() => {
               setIsEditing(true);
             }}
@@ -128,6 +134,31 @@ export default function MealHistoryClient({
         checkedList={checkedList}
         setCheckedList={setCheckedList}
       />
+      {showPopup && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+          <div className="flex w-80 flex-col items-center gap-2.5 rounded-2xl bg-white p-6 text-center shadow-xl">
+            <p className="text-grey-80 mb-2 leading-tight font-bold">
+              Are you sure you want to delete these meal record(s)?
+            </p>
+
+            <div className="flex w-full gap-2">
+              <button
+                onClick={() => setShowPopup(false)}
+                className="w-full rounded-2xl border border-red-100 py-3 font-bold text-red-100 hover:cursor-pointer"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={() => handleDelete()}
+                className="w-full rounded-2xl bg-red-100 py-3 font-bold text-white hover:cursor-pointer"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      <NavBar />
     </div>
   );
 }
