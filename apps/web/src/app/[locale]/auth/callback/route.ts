5e8b493ee29d535aee84ed00d8f060640bdcd119
Sasirasys <131147291+Sasirasys@users.noreply.github.com>
2026-04-16 16:46:59 +0700

    [FIX] setup flow (#70)
    * feat: prepare for calling recommender api
    
    * fix: show setup page if not done filling info
    + show info that user already inputted
    
    * fix: lint error
    
    ---------
    
    Co-authored-by: Vinita Jungwiwattanaporn <vinitajwp@gmail.com>

--- apps/web/src/app/[locale]/auth/callback/route.ts
+++ apps/web/src/app/[locale]/auth/callback/route.ts
@@ -5,13 +5,27 @@ export async function GET(request: Request) {
   const { searchParams, origin, pathname } = new URL(request.url);
   const locale = pathname.split("/")[1];
   const code = searchParams.get("code");
-  const next = searchParams.get("next") ?? `/${locale}/setup`;
+  const next = searchParams.get("next") ?? `/${locale}`;
 
   if (code) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
     if (!error) {
+      if (user) {
+        const { data, error } = await supabase
+          .from("user")
+          .select(`*`)
+          .eq("id", user.id)
+          .single();
+        if (!error && data.is_setup_finished === false) {
+          return NextResponse.redirect(`${origin}/${locale}/setup`);
+        }
+      }
       return NextResponse.redirect(`${origin}${next}`);
     }
   }
